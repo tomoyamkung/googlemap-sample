3d1eec10477c0b14ea58981a95a84a68e737ca05
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2014-03-23 18:22:06 +0900

    テストケースとなる JavaScript を保存したタイミングでテストを実行する設定を追加したのでコミット。

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,51 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		// パッケージファイルを読み込む
-		pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        // パッケージファイルを読み込む
+        pkg: grunt.file.readJSON('package.json'),
 
-		// grunt-contrib-connect の設定
-		connect: {
-			site: {
-				options: {
-					// ドメイン名は 'localhost' とする
-					hostname: 'localhost',
-					// 接続するポートは 9000 とする
-					port: 9000
-				}
-			}
-		},
+        // grunt-contrib-connect の設定
+        connect: {
+            live: { // LiveReload 用の設定
+                options: {
+                    hostname: 'localhost', port: 9000
+                }
+            },
+            qunit: { // QUnit 用の設定
+                options: {
+                    port: 9090, base: '.'
+                }
+            }
+        },
 
-		// grunt-contrib-watch の設定
-		watch: {
-			// プロジェクト下のある 全 HTML, CSS, JavaScript を監視対象とする
-			files: ['**/*.html', '**/*.css', '**/*.js'],
-			options: {
-				// 上記のファイルが更新されたらブラウザをリロードする
-				livereload: true
-			}
-		}
-	});
+        // grunt-contrib-watch の設定
+        watch: {
+        	live: { // LiveReload 用の設定
+				// プロジェクト下のある 全 HTML, CSS, JavaScript を監視対象とする
+	            files: ['**/*.html', '**/*.css', '**/*.js'],
+	            options: {
+	                livereload: true // 上記のファイルが更新されたらブラウザをリロードする
+	            }
+        	},
+            qunit: { // QUnit 用の設定
+	            files: ['test/**/*.js'], // test ディレクトリにある全 JavaScript を監視対象とする
+	            tasks: ['qunit']
+            }
+        },
 
-	// LiveReload するために必要なタスクをロードする
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+        // grunt-contrib-qunit の設定
+        qunit: {
+            all: ['test/**/*.html'] // test ディレクトリにある HTML を実行対象とする
+        }
+    });
 
-	// `grunt` コマンドで実行するデフォルトタスクを定義する
-	grunt.registerTask('default', ['connect', 'watch']);
+    // LiveReload するために必要なタスクをロードする
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt-contrib-qunit を有効にするためのタスクをロードする
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+
+    // `grunt` コマンドで実行するデフォルトタスクを定義する
+    grunt.registerTask('default', ['connect:live', 'watch:live']);
+    // `grunt test` で実行するタスクを定義する
+    grunt.registerTask('test', ['connect:qunit', 'watch:qunit', 'qunit']);
 };
--- test/stock-test.js
+++ test/stock-test.js
@@ -1,35 +1,62 @@
 var MockLocation = function(latitude, longitude) {
-	this.k = 1;
+	this.k = 1; // 緯度
 	if(latitude) {
 		this.k = latitude;
 	}
 
-	this.A = 10;
+	this.A = 10; // 経度
 	if(longitude) {
 		this.A = longitude;
 	}
 };
 
+module("MockLocation の生成に関するテスト");
 test("MockLocation の生成", function() {
 	var mock = new MockLocation();
-	ok(mock != 'undefined', "mock is not undefined.");
-	ok(mock.k === 1, "latitude is 1.");
-	ok(mock.A === 10, "longitude is 10.");
+	notStrictEqual(mock, undefined, "mock is not undefined.");
+	strictEqual(mock.k, 1, "latitude is 1.");
+	strictEqual(mock.A, 10, "longitude is 10.");
 
 	mock = new MockLocation(2, 20);
-	ok(mock != 'undefined', "mock is not undefined.");
-	ok(mock.k === 2, "latitude is 2.");
-	ok(mock.A === 20, "longitude is 20.");
+	notStrictEqual(mock, undefined, "mock is not undefined.");
+	strictEqual(mock.k, 2, "latitude is 2.");
+	strictEqual(mock.A, 20, "longitude is 20.");
 });
 
-test("InfoWindowStock の生成", function() {
-	var stock = new InfoWindowStock();
-	ok(stock != 'undefined', "インスタンスは生成される");
+
+var sut;
+module("InfoWindowStock の生成に関するテスト")
+QUnit.testStart(function() {
+	sut = new InfoWindowStock();
+})
+test("InfoWindowsut の生成", function() {
+	notStrictEqual(sut, undefined, "インスタンスは生成される");
 });
 
+
+module("InfoWindowStock に関するテスト")
+QUnit.testStart(function() {
+	sut = new InfoWindowStock();
+})
 test("ストックされていない状態で get する", function() {
+	var expected = sut.get(new MockLocation());
+	var actual = undefined;
+	strictEqual(expected, actual, "ストックしていないので undefined が返ってくる");
+});
+
+test("ストックしたものを取り出す", function() {
 	var location = new MockLocation();
-	var stock = new InfoWindowStock();
-	var x = stock.get(location);
-	ok(x == undefined, "undefined.");
+	sut.put(location, location);
+
+	var actual = sut.get(location);
+	propEqual(actual, location);
+});
+
+test("ストックしたものを get してもストックから削除されない", function() {
+	var location = new MockLocation();
+	sut.put(location, location);
+
+	var actual = sut.get(location); // 1回目の取り出し
+	actual = sut.get(location); // 2回目の取り出し
+	notStrictEqual(actual, undefined);
 });
